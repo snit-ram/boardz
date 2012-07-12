0f05de237e1d08633154033b31a0b8131eabb57f
snit.ram@gmail.com <snit.ram@gmail.com>
2012-07-12 09:37:19 -0300

    Adding more anctions and events

--- boardz.js
+++ boardz.js
@@ -1,16 +1,80 @@
 Boards = new Meteor.Collection("boards");
 
+function slug(str) {
+    str = str.replace(/^\s+|\s+$/g, ''); // trim
+    str = str.toLowerCase();
+
+    // remove accents, swap ñ for n, etc
+    var from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;";
+    var to   = "aaaaaeeeeeiiiiooooouuuunc------";
+    for (var i=0, l=from.length ; i<l ; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
+
+    str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+        .replace(/\s+/g, '-') // collapse whitespace and replace by -
+        .replace(/-+/g, '-'); // collapse dashes
+
+    return str;
+};
+
 if (Meteor.is_client) {
 
+    Template.main.content = function() {
+        return Template[Session.get('page')]();
+    };
+
     Template.menu.page_is = function(page) {
-        console.log(Session.get('page'), page);
-        return Session.get('page') == page;
+        return Session.equals('page', page);
     };
 
     Template.home.boards = function() {
-        var boards = Boards.find({});
-        boards.count();
+        var boards = Boards.find({}, {sort: {title: 1}}).fetch();
         return boards;
+    };
+
+    Template.boards.board = function() {
+        var name = Session.get('board-name');
+
+        return Boards.findOne({name: name}) || {};
+    };
+
+    Template.home.events = {
+        'click .add-board': function() {
+            $("#create-board-form").slideDown('slow');
+            $('.create-board-form .board-title').focus();
+        },
+
+        'click .create-board-form .close': function() {
+            $("#create-board-form").slideUp('slow');
+        },
+
+        'submit .create-board-form': function(event) {
+            event.preventDefault();
+
+            var title = $('input[name="board-title"]').val(),
+                name = slug(title);
+
+            Boards.insert({
+                title: title,
+                name: name
+            });
+        }
+    };
+
+    Template.boards.events = {
+        'click .add-card': function(event) {
+            var name = Session.get('board-name'),
+                board = Boards.findOne({name: name}),
+                column = event.target.dataset.column;
+
+            board[column] = board[column] || [];
+            board[column].push({
+                title: 'New card'
+            });
+
+            Boards.update({_id: board._id}, board);
+        }
     };
 
     Session.set('page', 'home');
@@ -25,19 +89,11 @@ if (Meteor.is_client) {
                 Session.set('page', 'about');
             },
 
-            '/new-board': function() {
-                $("#create-board-form").slideDown('slow');
-            },
-
             '/boards/:name': function(name) {
                 Session.set('page', 'boards');
-            },
+                Session.set('board-name', name);
+            }
         }).init();
-
-        Meteor.autosubscribe(function () {
-            var page = Session.get('page');
-            $('#container').html(Template[page]());
-        });
     });
 }
 
